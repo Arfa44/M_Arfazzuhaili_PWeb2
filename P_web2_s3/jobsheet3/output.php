8248dee3cb9d4622afc5cc0fa53f47e27a4c5940
M Arfazzuhaili <arfaprapen@gmail.com>
2024-09-17 22:05:23 +0700

    Add files via upload

---
+++ P_web2_s3/jobsheet3/output.php
@@ -0,0 +1,10 @@
+Program yang dijalankan akan menghasilkan output sebagai berikut :
+
+Mahasiswa: Lux Arcadia
+Dosen: Lucy
+A999
+D111
+Detail Kursus Online
+Detail Kursus Offline
+Proses pengajuan jurnal untuk Dosen
+Proses pengajuan jurnal untuk Mahasiswa
