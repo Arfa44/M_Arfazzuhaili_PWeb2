af09d3cb8f05ac0e67d100ab393cc2ba233707d8
M Arfazzuhaili <arfaprapen@gmail.com>
2024-09-17 22:00:39 +0700

    Delete P_web2_s3 directory

--- P_web2_s3/jobsheet1/output.php
+++
@@ -1,6 +0,0 @@
-Program yang dijalankan akan menghasilkan output sebagai berikut :
-
-Nama Mahasiswa = Ray Arcadia, NIM = 999 dari Jurusan JKB.
-Nama Mahasiswa = Sasuke, NIM = 1234 dari Jurusan JKB.
-Nama Mahasiswa = Ray Arcadia, NIM = 999 dari Jurusan TI.
-Nama Dosen = Noa, NIP = 56789, Mata Kuliah = Web Development.
--- P_web2_s3/jobsheet3/output.php
+++
@@ -1,6 +0,0 @@
-Program yang dijalankan akan menghasilkan output sebagai berikut :
-
-Student Name: Ali with ID: S12345 is a Student
-Teacher Name: Budi with ID: T54321 is a Teacher
-This is an online course.
-This is an offline course.
