ce01b581fed65f2321d99b22f414645d0a4402ee
M Arfazzuhaili <arfaprapen@gmail.com>
2024-09-18 14:01:22 +0700

    Delete P_web2_s3 directory

--- P_web2_s3/lat_jobsheet1/output.php
+++
@@ -1,7 +0,0 @@
-
-Program yang dijalankan akan menghasilkan output sebagai berikut :
-
-Mobil ini adalah Toyota berwarna Hitam.
-Mobil ini adalah Ferrari berwarna Red Devil.
-Buku: Pemrograman PHP, ditulis oleh John Doe.
-Buku: Journey of Arcadia, ditulis oleh Dex.
--- P_web2_s3/lat_jobsheet3/output.php
+++
@@ -1,8 +0,0 @@
-Program yang dijalankan akan menghasilkan output sebagai berikut :
-
-Buddy says Woof!
-Buddy says Woof!
-Kitty says Meow!
-Current Balance: 1300
-Area of Rectangle: 50
-Area of Circle: 153.9380400259
